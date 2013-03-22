579d98a905aa95d0558df7daa4f33fcd996b5edf
Andrew Cohen <acohen@brainscape.com>
2013-03-21 23:10:13 -0400

    postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillify::Application.config.secret_token = '4b5a2520a400ec622e0c4e4dd7d78409edecf5630300c85db40728e8f2704927a4dd9cc6e77b11b72d9fecf0b27d65d480fa8df73689ca8953663c7d4e3a4e2e'
